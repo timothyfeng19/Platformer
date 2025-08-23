2146e21f5fb208b210b398b198ce171616c99927
bradley121901 <bradley121901@gmail.com>
2025-08-23 12:04:51 -0400

    fix lint issues & restructure directories

---
+++ src/block_group.py
@@ -0,0 +1,24 @@
+import pygame
+import random
+from src.blocks import Block
+
+
+class BlockGroup():
+    def __init__(self):
+        self.all_blocks = pygame.sprite.Group()
+
+    def create(self, n):
+        self.num_blocks = n
+        bias = 1
+        prev_x = 0
+        for i in range(n):
+            if random.randint(0, 8) == 0:
+                bias = random.randint(0, 1) * 2 - 1
+            b = Block(
+                prev_x,
+                -(i * (70) + random.randint(-10, 10)) + 800,
+                random.randint(10, 150),
+                random.randint(10, 100)
+            )
+            prev_x += bias * random.randint(200, 300) + random.randint(-50, 50)
+            self.all_blocks.add(b)
---
+++ src/game.py
@@ -0,0 +1,47 @@
+import pygame
+from src.player import Player
+from src.block_group import BlockGroup
+from src.scoreboard import Scoreboard
+
+
+class Platformer:
+    def __init__(self):
+        pygame.init()
+        pygame.mixer.init()
+        self.screen = pygame.display.set_mode((1000, 800))
+        self.player = Player()
+        self.blocks = BlockGroup()
+        self.blocks.create(10)
+        self.all_blocks = (self.blocks.all_blocks)
+        self.scoreboard = Scoreboard()
+        self.camera_x = 0
+        self.camera_y = 0
+
+    def play(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_w:
+                        self.player.jump()
+
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_a]:
+                self.player.left(self.all_blocks)
+            if keys[pygame.K_d]:
+                self.player.right(self.all_blocks)
+
+            self.player.gravity(self.all_blocks)
+
+            self.scoreboard.update_score(500 - int(self.player.y))
+
+            self.screen.fill((0, 0, 0))
+
+            self.player.draw(self.screen)
+
+            self.all_blocks.draw(self.screen)
+
+            self.scoreboard.draw(self.screen)
+
+            pygame.display.flip()
